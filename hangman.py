c4959cd560d9c79a83ab214db1de1cdfb831ec75
NamiGoto <namigoto7@gmail.com>
2016-10-15 15:49:58 -1000

    commit message

---
+++ hangman.py
@@ -0,0 +1,9 @@
+import getpass
+def check_word():
+def check_letter():
+def main():
+	print("Welcome to Hangman!")
+	player1 = input("Enter Player 1's Name: ")
+	player2 = input("Enter Player 2's Name: ")
+	p1 = getpass.getpass("What do you want", player2, "to guess? ")
+	
